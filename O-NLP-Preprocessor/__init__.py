bd00a3e5dc9d245fa638825b99f7f91234b19d6a
Emeka Onyebuchi <chybyke.emeka@gmail.com>
2020-03-10 16:21:10 +0000

    first commit

---
+++ O-NLP-Preprocessor/__init__.py
@@ -0,0 +1,6 @@
+from O-NLP-Preprocessor.Stopwords import *
+from O-NLP-Preprocessor.Tokenize import *
+from O-NLP-Preprocessor.Stem import *
+from O-NLP-Preprocessor.Lemmatizer import * 
+
+import pandas as pd
---
+++ README.md
@@ -0,0 +1,3 @@
+This python package  has various libraries for processing NLP text. 
+
+While just specifying the pandas column, it automatically removes stopwords, tokenizes it, stems it and lemmatizes it
